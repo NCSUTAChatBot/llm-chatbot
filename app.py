70231d20d71bdbf87379ecabb26e24c6c0217668
Haoze Du <duhaoze1995@outlook.com>
2024-03-22 11:25:32 -0400

    merge fix

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, session
+from flask_session import Session
 from flask_cors import CORS
 from llmbackend import make_query
 
 app = Flask(__name__)
-CORS(app, resources={r"/ask": {"origins": ["192.168.86.29"]}})
+
+app.config["SECRET_KEY"] = "8zMym2xRX3*wRu&2"
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+CORS(app, supports_credentials=True, resources={r"/ask": {"origins": ["192.168.86.29"]}})
 chat_history = []
 @app.route('/')
 def index():
