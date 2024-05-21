82585a32c0cd1bb07b58acded072db4288bb72a8
skverma <skverma@ncsu.edu>
2024-05-20 23:32:32 -0400

    [Implement] Landing Page bar

--- saas-frontend/src/pages/landingpage.js
+++ saas-frontend/src/pages/landingpage.js
@@ -3,7 +3,7 @@
  * @author sanjitkverma (skverma)
  */
 
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../globalStyles.css';
 function LandingPage() {
@@ -13,8 +13,19 @@ function LandingPage() {
     const handleButtonClick = () => {
         navigate('/chat');
     };
+    const handleFeedback = () => {
+        window.open('https://forms.gle/5swJdyyfSdQxGww69');
+    };
+    // This hook is used to store the state of the help popup
+    const [showHelpPopup, setShowHelpPopup] = useState(false);
     return (
         <div className="landingPageContainer">
+            <div className="top-bar">
+                <h1 className="title">SAAS Chatbot @2024 NCSU CSC Dept </h1>
+                <div className="buttons">
+                    <button className="feedback-button" onClick={handleFeedback}>Leave Feedback</button>
+                </div>
+            </div>
             <div className="modalContainer">
                 <h3 className="modalHeader">Welcome</h3>
                 <p className="modalBodyText">This Chatbot supports asking questions from the textbook: <></>Engineering Software as a Service: An Agile Approach Using Cloud Computing
