5a2c652a885904fc68d4cc042a3c7182b6afe57f
amritav <amritavisalam@gmail.com>
2024-09-23 15:44:42 -0400

    remove unused code for upload button on sidebar

--- saas-frontend/src/pages/courseEvaluation/CEChatpage.js
+++ saas-frontend/src/pages/courseEvaluation/CEChatpage.js
@@ -342,7 +342,7 @@ const ChatPage = () => {
     const storedUserInfo = localStorage.getItem("userInfo");
     if (storedUserInfo) {
       setUserInfo(JSON.parse(storedUserInfo));
-    } 
+    }
   }, [navigate]);
 
   const toggleDropdown = () => setShowDropdown(!showDropdown);
@@ -459,35 +459,26 @@ const ChatPage = () => {
               />
             </svg>
           </button>
-          <input
-            type="file"
-            id="file-upload"
-            style={{ display: "none" }}
-            onChange={handleFileChange}
-            accept=".csv, .xlsx"
-          />
-          {(
-            <button
-              className="refresh-button"
-              onClick={handleDownloadChat}
-              title="Download Chat"
+          <button
+            className="refresh-button"
+            onClick={handleDownloadChat}
+            title="Download Chat"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              style={{ width: "22px", height: "22px" }}
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                style={{ width: "22px", height: "22px" }}
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3"
-                />
-              </svg>
-            </button>
-          )}
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3"
+              />
+            </svg>
+          </button>
         </div>
         <div className="uploaded-files">
           {uploadingFiles.map((file, index) => (
